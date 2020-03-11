1fb54affbed914dff5edf8d42bbd559e00ac803a
channing <402820553@qq.com>
2020-03-11 23:14:39 +0800

    新建APP

--- chinglish/urls.py
+++ chinglish/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url
-from chinglish import view
+from web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', view.hello),
+    path('hello/', views.hello),
 ]
